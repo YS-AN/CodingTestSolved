e56cfbed2a2e7a6d0ac63f73daa964c566f78502
YS AN <as5948@soongsil.ac.kr>
2023-01-03 23:52:49 +0900

    Day03
    sort 완료

--- Lv0_EnterCoding/Day03.js
+++ Lv0_EnterCoding/Day03.js
@@ -4,52 +4,41 @@ function sol_GetRemain(num1, num2) {
 }
 
 //중앙값 구하기
+function GetMiddleValue(array) {
+    let len = array.length - 1;
+    //let sortedArr = quickSort(0, len, array);
+    //return sortedArr[parseInt(len / 2)];
+    //Quick Sort는 시간 제한으로 빠꾸당함
 
-
-
-function solution(array) {
-    let len = array.length;
-    let sortedArr = quickSort(0, len - 1, array);
-
-    return sortedArr[parseInt(len / 2)];
+    array.sort((a, b) => a - b); //a-b는 오름차순 b-a는 내림차순
+    return array[parseInt(len / 2)];
 }
 
 function quickSort(l, r, arr) {
     if (l < r) {
-        let p = partition(l, r, arr, "P");
-        console.log("=============== " + p + " ==============")
-        quickSort(l, p - 1, arr, "L");
-        quickSort(p + 1, r, arr, "R")
-    }
+        let p = partition(l, r, arr);
 
+        quickSort(l, p - 1, arr);
+        quickSort(p + 1, r, arr);
+    }
     return arr;
 }
 
-function partition(l, r, arr, tpye) {
-    console.log("==========[ " + tpye + " partition=]==========");
-    console.log("=============" + l + " / " + r + "=============");
-
-    let p = l;
+function partition(l, r, arr) {
+    let pivot = arr[l];
 
     while (l < r) {
-        if (arr[p] > arr[r]) {
-            let tmp = arr[l];
-            arr[l] = arr[r];
-            arr[r] = tmp;
-
-            l += 1;
-        }
-        else {
+        while (pivot < arr[r]) {
             r -= 1;
         }
+        while (pivot > arr[l] && r > l) {
+            l += 1;
+        }
+
+        let tmp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = tmp;
     }
-
-    var str = "";
-    arr.forEach((item) => { str += "  " + item });
-    console.log(str);
-    return r - 1;
+    return l;
 }
-
-solution([3, 0, 1, 8, 7, 2, 5, 4]);
-
-//2 0 1 8 7 3 5 4
+console.log(GetMiddleValue([3, 0, 1, 8, 7, 2, 5, 4]));
